c704cc3434841f64de5fe8ef7baef79760c9daf9
Yvan Lopez <ylopez@getontop.com>
2024-07-04 02:05:24 -0500

    ms usuarios ya se conecta con la base de datos que esta en un multi-container.

--- msvc-cursos/src/main/java/com/yvancho/springcloud/msvc/cursos/infrastructure/adapters/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/com/yvancho/springcloud/msvc/cursos/infrastructure/adapters/clients/UsuarioClientRest.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "msvc-usuarios", url = "localhost:8001")
+@FeignClient(name = "msvc-usuarios", url = "${msvc.usuarios.url}")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
